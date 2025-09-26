33742a6cd2c12d789fd8f253f1ed31c4a3741271
Liam J <wjester883@gmail.com>
2025-09-26 18:04:44 +0000

    Lesson02 and Lesson03

---
+++ lesson02.py
@@ -0,0 +1,32 @@
+Jotaro = "hello world"
+Password = "Dioda"
+Email = "Diotaro@gmail.com"
+print("Password: ",Password)
+print("Email: ",Email)
+temperature = 67.67
+print(type(temperature))
+enabled = False
+complete = True
+print( complete)
+print(enabled)
+x = 1
+y = -1
+print(x * y)
+
+X = "Xavier John Dinkleburg"
+Y= "Youggle Von Dinkleburg"
+Z = "Zorgin George Dinkleburg"
+
+C = "count"
+count = 10
+F = 5
+print(count-F)
+
+X = 4
+Y = "hello"
+X = Y
+Y = X
+
+is_raining = False
+is_raining = True
+print("Is It raining?:", is_raining)
