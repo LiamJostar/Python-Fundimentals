d9265f012d157a946b4f4803e87002e48326c514
Liam J <wjester883@gmail.com>
2025-10-09 19:07:26 +0000

    input

--- lesson05_input.py
+++ lesson05_input.py
@@ -1,24 +1,27 @@
 import math
 import random
 
-# print("What is your name?")
-# name = input("Enter user:")
-# print("Welcome back,",name)
+print("What is your name?")
+name = input("Enter user:")
+print("Welcome back,",name)
 
-# print("What is your favorite color?")
-# color = input("Enter color:")
-# print("You picked:", color)
+print("What is your favorite color?")
+color = input("Enter color:")
+print("You picked:", color)
 
-# print("Give me two numbahs")
-# number1 = int(input("Number 1:"))
-# number2 = int(input("Number 2:"))
-# print(number1 + number2)
+print("Give me two numbahs")
+number1 = int(input("Number 1:"))
+number2 = int(input("Number 2:"))
+print(number1 + number2)
 
 print("Give me the diameter of a circle")
 diameter = int(input("Diameter:"))
-result = float(math.pi(diameter/2)**2) 
+result = math.pi(diameter/2)**2 
 print("Area:", result)
 
+dicenumber = int(input("How many sides?"))
+randomnumber = random.randint(1,dicenumber)
+print("Result:", randomnumber)
 
 
 
@@ -30,11 +33,10 @@ print("Area:", result)
 
 
 
-
-# sides = int(input("Who is the protagonist?: "))
-# sides = math.floor(sides)
-# if sides < 1:
-#     print("Cannot be negitive")
-# else:
-#     chooserandomside = random.randint(1, sides) 
-# print("Your character is: ",chooserandomside)
+sides = int(input("Who is the protagonist?: "))
+sides = math.floor(sides)
+if sides < 1:
+    print("Cannot be negitive")
+else:
+    chooserandomside = random.randint(1, sides) 
+print("Your character is: ",chooserandomside)
